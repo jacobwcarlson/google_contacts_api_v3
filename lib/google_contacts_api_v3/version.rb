e128834281f6b5a1d30b8b13342b3e61e50fd27e
Jacob W Carlson <the.real.jacobwcarlson@gmail.com>
2012-12-14 11:09:40 -0800

    Revert "Forgot to bump patch number."
    This reverts commit 436a5fd7e49f1b93d25da035b125d4bd49bb414f.
    
    Bad idea jeans.

--- lib/google_contacts_api_v3/version.rb
+++ lib/google_contacts_api_v3/version.rb
@@ -2,7 +2,7 @@ module GoogleContactsApiV3
   class Version #:nodoc:
     MAJOR  = 0
     MINOR  = 1
-    PATCH  = 4
+    PATCH  = 3
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end # Version
   
