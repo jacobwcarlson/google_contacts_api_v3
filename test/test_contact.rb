8bb3954c44b32117fff57bfe18980e860f356588
Jacob Carlson <twitch@Jacobs-MacBook-Pro.local>
2013-12-03 11:42:20 -0800

    Finished these.

--- test/test_contact.rb
+++ test/test_contact.rb
@@ -14,19 +14,26 @@ class TestContact < MiniTest::Unit::TestCase
       contact = Contact.create_from_json(json_contact).tap do |contact|
         assert_equal contact.title, json_contact['title'].andand['$t']
         assert_equal contact.notes, json_contact['content'].andand['$t']
-        assert_equal contact.birthday, json_contact['gContact$birthday'].andand['when']
-        assert_equal contact.nickname, json_contact['gContact$nickname'].andand['$t']
+        assert_equal contact.birthday,
+          json_contact['gContact$birthday'].andand['when']
+        assert_equal contact.nickname,
+          json_contact['gContact$nickname'].andand['$t']
 
-        assert_equal contact.organizations.size, json_contact['gd$organization'].size
-        assert_equal contact.email_addresses.size, json_contact['gd$email'].size
-        assert_equal contact.phone_numbers.size, json_contact['gd$phoneNumber'].size
-        assert_equal contact.postal_addresses.size == json_contact['gd$structredPostalAddress'].size
+        assert_equal contact.organizations.size,
+          json_contact['gd$organization'].size
+        assert_equal contact.email_addresses.size,
+          json_contact['gd$email'].size
+        assert_equal contact.phone_numbers.size,
+          json_contact['gd$phoneNumber'].size
+        assert_equal contact.postal_addresses.size,
+          json_contact['gd$structuredPostalAddress'].size
 
         json_contact['gContact$event'].each_with_index do |gd_event, idx|
           contact.events[idx].tap do |event|
             assert_equal event.rel, gd_event['rel']
             assert_equal event.label, gd_event['label']
-            assert_equal event.when, DateTime.parse(gd_event['gd$when']['startTime'])
+            assert_equal event.when,
+              DateTime.parse(gd_event['gd$when']['startTime'])
 
             assert event.frozen?
           end
@@ -34,9 +41,9 @@ class TestContact < MiniTest::Unit::TestCase
 
         json_contact['gContact$website'].each_with_index do |gd_site, idx|
           contact.websites[idx].tap do |website|
-            assert_equal website.href, gs_site['href']
-            assert_equal website.rel, gs_site['rel']
-            assert_equal website.label, gs_site['label']
+            assert_equal website.href, gd_site['href']
+            assert_equal website.rel, gd_site['rel']
+            assert_equal website.label, gd_site['label']
 
             assert website.frozen?
           end
